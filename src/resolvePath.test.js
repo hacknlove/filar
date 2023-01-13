feae1bd1e18ad5237f59147e1faeb3515ef46ed7
hacknlove <antonio@hacknlove.org>
2023-01-13 17:29:35 +0100

    test: fix test

--- src/resolvePath.test.js
+++ src/resolvePath.test.js
@@ -2,8 +2,8 @@ const { resolvePath } = require('./resolvePath');
 
 describe('resolvePath', () => {
     it('should return the correct path', () => {
-        expect(resolvePath('/')).toBe(`${process.cwd()}/route.index.html`);
-        expect(resolvePath('/about')).toBe(`${process.cwd()}/route.about.html`);
-        expect(resolvePath('/about/')).toBe(`${process.cwd()}/route.about/index.html`);
+        expect(resolvePath('/')).toBe(`${process.cwd()}/index.route.html`);
+        expect(resolvePath('/about')).toBe(`${process.cwd()}/about.route.html`);
+        expect(resolvePath('/about/')).toBe(`${process.cwd()}/about/index.route.html`);
     });
 });
