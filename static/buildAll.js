dde7239170159ecd40dfc38cf6c4180b9ca06dba
hacknlove <antonio@hacknlove.org>
2023-02-24 12:27:46 +0100

    fix: type from sc to ce (custom element)

--- static/buildAll.js
+++ static/buildAll.js
@@ -5,7 +5,7 @@ const { build } = require("./build");
 async function buildAll({ from, to }) {
   const files = await globAsync("**/*.html", {
     cwd: from,
-    ignore: "**/*.{se,sc}.html",
+    ignore: "**/*.{se,ce}.html",
   });
 
   return Promise.all(files.map((filePath) => build({ from, to, filePath })));
