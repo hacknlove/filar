2f31cc96eee5b9269fce37df38c87c337407bc68
hacknlove <antonio@hacknlove.org>
2023-03-05 09:58:32 +0100

    feat: clears build directory

--- cli/build.js
+++ cli/build.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+const { rmdir } = require('node:fs/promises');
+const { join } = require('node:path');
+
 const { from } = require('./argv');
 
 console.log(`Building from ${from}`);
@@ -8,9 +11,10 @@ const { buildAll } = require("../server/build/all");
 const { initializeServerElements } = require("../server/se/initialize");
 
 async function main() {
+  await rmdir(join(from, '.build'), { recursive: true, force: true });
   await initializeServerElements();
 
   return buildAll();
 }
 
-main();
+module.exports = main();
--- cli/build.test.js
+++ cli/build.test.js
@@ -1,14 +1,24 @@
 const { buildAll } = require("../server/build/all");
 const { initializeServerElements } = require("../server/se/initialize");
 const { watchServerElements } = require("../server/se/watch");
+const { rmdir } = require('node:fs/promises');
 
 jest.mock("../server/se/initialize");
 jest.mock("../server/build/all");
 jest.mock("../server/se/watch");
 
+jest.mock('node:fs/promises', () => ({
+  rmdir: jest.fn(),
+}));
+
+jest.mock('./argv', () => ({
+  from: 'test',
+}));
+
 test("main no watch", async () => {
   await require("../cli/build");
 
+  expect(rmdir).toHaveBeenCalledWith('test/.build', { recursive: true, force: true });
   expect(initializeServerElements).toHaveBeenCalledTimes(1);
   expect(buildAll).toHaveBeenCalledTimes(1);
   expect(watchServerElements).toHaveBeenCalledTimes(0);
