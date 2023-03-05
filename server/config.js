4afcf6fdbb0d3d16c813d02ab1a4ac8d17a9993f
hacknlove <antonio@hacknlove.org>
2023-03-05 11:35:42 +0100

    chore: config

--- server/config.js
+++ server/config.js
@@ -1,8 +1,1 @@
-module.exports = {
-    from: process.env.FROM || process.cwd(),
-    port: process.env.PORT || 3000,
-    host: process.env.HOST || 'localhost',
-    watch: process.env.WATCH || false,
-    livereload: process.env.LIVERELOAD || false,
-    ... JSON.parse(process.env.CONFIG || '{}')
-}
+module.exports = {};
