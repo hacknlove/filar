5b57f4b4edf19a6f2816d8cf6f566b4005e923c7
hacknlove <antonio@hacknlove.org>
2023-03-09 08:36:57 +0100

    feat: adds a helper for resolving srcs

---
+++ server/common/resolveSrc.js
@@ -0,0 +1,31 @@
+const { join, resolve } = require("path");
+
+function resolveSrc ({ src, relative, root }) {
+    if (!relative.startsWith(root)) {
+        relative = join(root, relative);
+    }
+
+    if (src === null) {
+        return root;
+    }
+    if (src === "") {
+        return relative;
+    }
+
+    let resolved
+
+
+    if (src.startsWith("/")){
+        resolved = resolve(root, src.substring(1));
+    } else {
+        resolved = resolve(relative, src);
+    }
+
+    if (resolved.startsWith(root)) {
+        return resolved;
+    }
+
+    throw new Error(`Not allowed to resolve path outside of ${root}`);
+}
+
+exports.resolveSrc = resolveSrc;
