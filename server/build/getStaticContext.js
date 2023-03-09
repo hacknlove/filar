51540b179fc5264fc730345b4ecc181302d0da01
hacknlove <antonio@hacknlove.org>
2023-03-09 07:23:10 +0100

    chore: prettifier

--- server/build/getStaticContext.js
+++ server/build/getStaticContext.js
@@ -2,35 +2,39 @@ const config = require("../config");
 const { isAModule } = require("../common/isAModule");
 
 async function getStaticContext(filePath) {
-    const importPath = filePath.substring(0, filePath.length - 5); //.html length is 5
+  const importPath = filePath.substring(0, filePath.length - 5); //.html length is 5
 
-    const context = {
+  const context = {
+    filePath,
+  };
+  if (isAModule(importPath)) {
+    const requiredContext = require(importPath);
+    if (typeof requiredContext === "function") {
+      Object.assign(
+        context,
+        requiredContext({
+          filePath,
+          config,
+        })
+      );
+    } else {
+      Object.assign(context, requiredContext);
+    }
+  }
+
+  if (typeof config.context === "function") {
+    Object.assign(
+      context,
+      config.context({
         filePath,
-    }
-    if (isAModule(importPath)) {
-        const requiredContext = require(importPath);
-        if (typeof requiredContext === 'function') {
-            Object.assign(context, requiredContext({
-                filePath,
-                config
-            }));
-        } else {
-            Object.assign(context, requiredContext);
-        }
-    }
+        config,
+      })
+    );
+  } else if (typeof config.context === "object") {
+    Object.assign(context, config.context);
+  }
 
-    if (typeof config.context === 'function') {
-        Object.assign(context, config.context(
-            {
-                filePath,
-                config
-            }
-        ));
-    } else if (typeof config.context === 'object') {
-        Object.assign(context, config.context);
-    }
-
-    return context;
+  return context;
 }
 
 exports.getStaticContext = getStaticContext;
--- server/build/getStaticContext.test.js
+++ server/build/getStaticContext.test.js
@@ -1,81 +1,92 @@
 const { getStaticContext } = require("./getStaticContext");
-const { isAModule } = require('../common/isAModule');
+const { isAModule } = require("../common/isAModule");
 const config = require("../config");
 
-jest.mock('../common/isAModule');
+jest.mock("../common/isAModule");
 
-jest.mock('../config', () => ({ }));
+jest.mock("../config", () => ({}));
 
-const filePath = '../../test/context.html';
+const filePath = "../../test/context.html";
 
-describe('getStaticContext', () => {
-    afterEach(() => {
-        jest.clearAllMocks();
-        delete config.context;
+describe("getStaticContext", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+    delete config.context;
+  });
+
+  it("should return an object", async () => {
+    expect(await getStaticContext(filePath)).toBeInstanceOf(Object);
+  });
+
+  it("should include filePath in the context", async () => {
+    expect(await getStaticContext(filePath)).toHaveProperty(
+      "filePath",
+      filePath
+    );
+  });
+
+  it("should call the context function in config, passing filePath and config", async () => {
+    config.context = jest.fn();
+
+    await getStaticContext(filePath);
+
+    expect(config.context).toHaveBeenCalledWith({ filePath, config });
+  });
+
+  it("should include the result of the context function in the context", async () => {
+    config.context = jest.fn(() => ({ someData: true }));
+
+    expect(await getStaticContext(filePath)).toHaveProperty("someData", true);
+  });
+
+  it("should include the context object in config in the context", async () => {
+    config.context = { someData: true };
+
+    expect(await getStaticContext(filePath)).toHaveProperty("someData", true);
+  });
+
+  it("should call the requiredContext function, passing filePath and config", async () => {
+    isAModule.mockReturnValue(true);
+    const mockRequiredContext = jest.fn(() => ({ someData: true }));
+    jest.mock("../../test/context", () => mockRequiredContext, {
+      virtual: true,
     });
 
-    it('should return an object', async () => {
-        expect(await getStaticContext(filePath)).toBeInstanceOf(Object);
+    await getStaticContext(filePath);
+
+    expect(mockRequiredContext).toHaveBeenCalledWith({ filePath, config });
+  });
+
+  it("should include the result of the requiredContext function in the context", async () => {
+    isAModule.mockReturnValue(true);
+    const mockRequiredContext = jest.fn(() => ({ someData: true }));
+    jest.mock("../../test/context", () => mockRequiredContext, {
+      virtual: true,
     });
 
-    it('should include filePath in the context', async () => {
-        expect(await getStaticContext(filePath)).toHaveProperty('filePath', filePath);
+    expect(await getStaticContext(filePath)).toHaveProperty("someData", true);
+  });
+
+  it("should include the requiredContext object in the context", async () => {
+    isAModule.mockReturnValue(true);
+    const mockRequiredContext = { someData: true };
+    jest.mock("../../test/context", () => mockRequiredContext, {
+      virtual: true,
     });
 
-    it('should call the context function in config, passing filePath and config', async () => {
-        config.context = jest.fn();
+    const context = await getStaticContext(filePath);
 
-        await getStaticContext(filePath);
+    expect(context).toHaveProperty("someData", true);
+  });
 
-        expect(config.context).toHaveBeenCalledWith({ filePath, config });
+  it("should assign requiredContext to context when it is not a function", async () => {
+    const mockRequiredContext = { someOtherData: true };
+    isAModule.mockReturnValue(true);
+    jest.mock("../../test/context2", () => mockRequiredContext, {
+      virtual: true,
     });
+    const context = await getStaticContext("../../test/context2.html");
 
-    it('should include the result of the context function in the context', async () => {
-        config.context = jest.fn(() => ({ someData: true }));
-
-        expect(await getStaticContext(filePath)).toHaveProperty('someData', true);
-    });
-
-    it('should include the context object in config in the context', async () => {
-        config.context = { someData: true };
-
-        expect(await getStaticContext(filePath)).toHaveProperty('someData', true);
-    });
-
-    it('should call the requiredContext function, passing filePath and config', async () => {
-        isAModule.mockReturnValue(true);
-        const mockRequiredContext = jest.fn(() => ({ someData: true }));
-        jest.mock('../../test/context', () => mockRequiredContext, { virtual: true });
-
-        await getStaticContext(filePath);
-
-        expect(mockRequiredContext).toHaveBeenCalledWith({ filePath, config });
-    });
-
-    it('should include the result of the requiredContext function in the context', async () => {
-        isAModule.mockReturnValue(true);
-        const mockRequiredContext = jest.fn(() => ({ someData: true }));
-        jest.mock('../../test/context', () => mockRequiredContext, { virtual: true });
-
-        expect(await getStaticContext(filePath)).toHaveProperty('someData', true);
-    });
-
-    it('should include the requiredContext object in the context', async () => {
-        isAModule.mockReturnValue(true);
-        const mockRequiredContext = { someData: true };
-        jest.mock('../../test/context', () => mockRequiredContext, { virtual: true });
-
-        const context = await getStaticContext(filePath);
-
-        expect(context).toHaveProperty('someData', true);
-    });
-
-    it('should assign requiredContext to context when it is not a function', async () => {
-        const mockRequiredContext = { someOtherData: true };
-        isAModule.mockReturnValue(true);
-        jest.mock('../../test/context2', () => mockRequiredContext, { virtual: true });
-        const context = await getStaticContext('../../test/context2.html');
-
-        expect(context).toHaveProperty('someOtherData', true);
-    });
+    expect(context).toHaveProperty("someOtherData", true);
+  });
 });
